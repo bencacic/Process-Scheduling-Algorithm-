ece809bcc7092ac89b894537a9385463f67ae730
Khanh Le <lekhanhsone@gmail.com>
2023-11-16 15:08:01 -0700

    Process.c created

---
+++ processes.c
@@ -0,0 +1,7 @@
+/*
+    Group members: Mohammed Cacic, Khanh Le, Ben Cacic
+    Course: COMP 3659
+    Assignment 2: Process Scheduling Algorithm Simulation 
+    File Name: processes.c
+    Purpose use: Store the information of the processes
+*/
