bd43cae5bdbc5ba25b12bd2430d6e60a897fdcad
Khanh Le <lekhanhsone@gmail.com>
2023-11-16 15:06:59 -0700

    new edit

--- scheduling.c
+++ scheduling.c
@@ -2,6 +2,8 @@
     Group members: Mohammed Cacic, Khanh Le, Ben Cacic
     Course: COMP 3659
     Assignment 2: Process Scheduling Algorithm Simulation 
+    File Name: scheduling.c
+    Purpose use: Store scheduling algorithms
 */
 #include <stdio.h>
 
