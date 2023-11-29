a6c1f80d2f933451b1f16f88a1cf079f7efeba23
Khanh Le <76984067+kle6951@users.noreply.github.com>
2023-11-28 17:04:36 -0700

    Delete processes.c

--- processes.c
+++
@@ -1,9 +0,0 @@
-/*
-    Group members: Mohammed Cacic, Khanh Le, Ben Cacic
-    Course: COMP 3659
-    Assignment 2: Process Scheduling Algorithm Simulation 
-    File Name: processes.c
-    Purpose use: Store the information of the processes
-*/
-
-void testing();
