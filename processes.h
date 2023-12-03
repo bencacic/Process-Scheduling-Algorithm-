255499d3a113f6c6a746573d111cf637f7fc1760
Khanh Le <76984067+kle6951@users.noreply.github.com>
2023-12-03 15:14:05 -0700

    Add files via upload

---
+++ processes.h
@@ -0,0 +1,37 @@
+/*
+    Group members: Mohammed Cacic, Khanh Le, Ben Cacic
+    Course: COMP 3659
+    Assignment 2: Process Scheduling Algorithm Simulation 
+    File Name: processes.h
+    Purpose use: Store the information of the processes
+*/
+#ifndef PROCESSES_H
+#define PROCESSES_H
+
+#include "mylib.h"
+
+typedef struct {
+    int process_id;
+    int arrival_time;
+    int burst_time;
+    int priority;  
+    int actualStart;
+    float estimatedBurstTime;
+    int completed;
+    int prevBursts[4];
+} Process;
+
+void initProcesses(Process process[]);
+
+int compareArrivalTime(const void *a, const void *b);
+int compareSJF(const void *a, const void *b);
+int comparePriority (const void *a, const void *b);
+void readyQueue (Process process[], int choice) ;
+void sort_FCFS (Process process[]);
+void sort_RR ();
+void sort_PR(Process process[]);
+void sort_SJB (Process process[]);
+void estimateBurstTime(Process process[]);
+
+
+#endif
