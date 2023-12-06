9407c829e1ad0d45c0012b2b5b45a51275d9a45a
Khanh Le <lekhanhsone@gmail.com>
2023-12-06 15:32:09 -0700

    new edit to stat_table.c from Khanh

--- stat_table.h
+++ stat_table.h
@@ -17,5 +17,6 @@ static void print_table_ExpectedBurst(Process process[]);
 static void print_label(int choice);
 static void print_lable_name();
 static void print_table_RR(Process process[]);
+static void print_process_stat(Process process[], int index, int choice);
 
 #endif
